5182f5a2a637048ad00a042b2d5df673ebff3827
rahul <rahulkadam1287@gmail.com>
2025-01-24 19:50:03 +0530

    test

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -28,7 +28,7 @@ terraform {
 
   backend "s3" {
     bucket = "gitopsproject2025"
-    key    = "s3://gitopsproject2025/terraform.tfstate/"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 
